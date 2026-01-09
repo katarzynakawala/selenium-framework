0eacdecc2eaebda70bbee1324a7085dddbc9e7f2
Kasia Kawala <kat.kawala@gmail.com>
2026-01-09 16:45:13 +0100

    [test] adding negative test scenario

--- src/test/java/com/ui/pages/LoginPage.java
+++ src/test/java/com/ui/pages/LoginPage.java
@@ -18,6 +18,7 @@ public class LoginPage extends BrowserUtility {
     private static final By EMAIL_TEXT_BOX_LOCATOR = By.id("email");
     private static final By PASSWORD_TEXT_BOX_LOCATOR = By.id("passwd");
     private static final By SUBMIT_LOGIN_BUTTON_LOCATOR = By.id("SubmitLogin");
+    private static final By ERROR_MESSAGE_LOCATOR = By.cssSelector("div.alert.alert-danger");
 
 
     public MyAccountPage doLoginWith(String emailAddress, String password) {
@@ -27,5 +28,17 @@ public class LoginPage extends BrowserUtility {
         clickOn(SUBMIT_LOGIN_BUTTON_LOCATOR);
         MyAccountPage myAccountPage = new MyAccountPage(getDriver());
         return myAccountPage;
+    }
+
+    public LoginPage LoginWithInvalidCredentials(String emailAddress, String password) {
+        log.info("Attempting login for user with invalid credentials '{}'", emailAddress);
+        enterText(EMAIL_TEXT_BOX_LOCATOR, emailAddress);
+        enterText(PASSWORD_TEXT_BOX_LOCATOR, password);
+        clickOn(SUBMIT_LOGIN_BUTTON_LOCATOR);
+        return new LoginPage(getDriver());
+    }
+
+    public String getErrorMessage() {
+        return getVisibleText(ERROR_MESSAGE_LOCATOR);
     }
 }
